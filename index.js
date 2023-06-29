d7bcae3370845a2dcbf25091bc4faeefed830f6d
Ayush Pratap Singh <ayushdce1@gmail.com>
2023-06-29 16:12:16 +0530

    updated

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-document.getElementsByClassName("click_here")[0].addEventListener("click",()=>{
-    document.getElementsByClassName("pop_up")[0].classList.toggle("pop_active");
-})
-document.addEventListener("click",(e)=>{
-   
-    if(e.target.closest(".cross") || e.target.closest(".save") || e.target.closest(".cancel")){
-        document.getElementsByClassName("pop_up")[0].classList.toggle("pop_active");
-    }
-   
-    if(e.target.closest(".cross") || e.target.closest(".pop_up") || e.target.closest(".click_here")){
-
-    }else{
-        document.getElementsByClassName("pop_up")[0].classList.toggle("pop_active");
-    }
-   
-    
-})
+document.addEventListener("click", (e) => {
+  if (
+    e.target.closest(".cross") ||
+    e.target.closest(".save") ||
+    e.target.closest(".cancel")
+  ) {
+    document.getElementsByClassName("pop_up")[0].classList.remove("pop_active");
+  } else if (e.target.closest(".click_here")) {
+    document.getElementsByClassName("pop_up")[0].classList.add("pop_active");
+  } else if (e.target.closest(".pop_up")) {
+  } else {
+    document.getElementsByClassName("pop_up")[0].classList.remove("pop_active");
+  }
+});
